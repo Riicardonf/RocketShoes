dc7ba1d27ebcc45c3d66bda5686489181bb67fe6
Ricardo <ricardobatistanf@gmail.com>
2020-02-17 23:13:01 -0300

    Config API with JSON.server/ Styling the app

---
+++ src/pages/Cart/index.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
+
+import { Container, ProductTable, Total } from './styles';
+
+export default function Cart() {
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/camisa-flamengo-toquio-n10-masculina/12/D40-1285-012/D40-1285-012_detalhe1.jpg?resize=280:280"
+                alt="camisa"
+              />
+            </td>
+            <td>
+              <strong>Tênis muito massa</strong>
+              <span>R$249,90</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={2} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$500,00</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+      <footer>
+        <button type="button">Finalizar Pedido</button>
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$1920,28</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
+}
